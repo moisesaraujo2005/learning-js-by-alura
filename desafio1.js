bed212952b5e754d57a3abf0052f7bf65c8d9b5b
moisesaraujo2005 <moises.silva@sptech.school>
2024-06-01 12:21:57 -0300

    Conhecendo arrays

---
+++ desafio1.js
@@ -0,0 +1,16 @@
+//Sem vetor
+const nota1= 10;
+const nota2= 6.5;
+const nota3= 8;
+const nota4 = 7.5;
+
+const media = (nota1+nota2+nota3+nota4) / 4 
+
+console.log(media)
+
+
+const notas = [10,6.5,8,7.5]
+
+const mediaVetor = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+console.log (mediaVetor)
